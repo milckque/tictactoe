0f7444b9c939f1263052118693456b2ca191683c
Lionel <152048641+milckque@users.noreply.github.com>
2024-11-24 17:24:32 +1100

    implemented board drawing function

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -2,6 +2,22 @@
 #include <iostream>
 using namespace std;
 
+void drawBoard(char board[3][3]) {
+    cout << "-------------\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << "| ";
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j] << " | ";
+        }
+        cout << "\n-------------\n";
+    }
+}
+
+
 int main() {
-    cout << "Hello World!" << endl;
+    char board[3][3] = { { ' ', ' ', ' ' },
+                         { ' ', ' ', ' ' },
+                         { ' ', ' ', ' ' } };
+
+    drawBoard(board);
 }
